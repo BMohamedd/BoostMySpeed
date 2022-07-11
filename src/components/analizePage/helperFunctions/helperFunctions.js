4e9328f5c47059015f7a029dfaf69ff29eba9d63
BMohamedd <89010936+BMohamedd@users.noreply.github.com>
2022-07-11 16:15:43 +0200

    removed some unwanted lines of code (causing errors)

--- src/components/analizePage/helperFunctions/helperFunctions.js
+++ src/components/analizePage/helperFunctions/helperFunctions.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-<<<<<<< HEAD
+
 const mobileUrl =
   "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBF4t5ZpUfX5g82nb7zpEYX1KOP4j3liCI&category=ACCESSIBILITY&category=BEST_PRACTICES&category=PERFORMANCE&category=SEO&strategy=MOBILE&url=";
 const disktopUrl =
   "https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?key=AIzaSyBF4t5ZpUfX5g82nb7zpEYX1KOP4j3liCI&category=ACCESSIBILITY&category=BEST_PRACTICES&category=PERFORMANCE&category=SEO&strategy=DESKTOP&url=";
-=======
-const backEndUrl = "https://speed-speed-speed.herokuapp.com/report?url=";
->>>>>>> 5486a3e46dc238a88169a96fd6f8a2a2ab9d6e5e
+
 
 export const finalUrlCheck = (url) => {
   // checks if the url exists
